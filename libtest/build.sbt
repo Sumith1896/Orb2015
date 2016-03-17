baeb451ca51f141d966a47c6f64dc278b983a0e7
ravi <ravi.kandhadai@epfl.ch>
2016-03-17 18:36:44 +0100

    Using macmeoize library to realize memoization

---
+++ libtest/build.sbt
@@ -0,0 +1,10 @@
+name := "LeonLibraryExection"
+
+version := "1.0"
+
+organization := "ch.epfl.lara"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "com.softwaremill.macmemo" %% "macros" % "0.3"
+
---
+++ libtest/project/Build.scala
@@ -0,0 +1,39 @@
+import sbt._
+import Keys._
+
+object BuildSettings {
+  val paradiseVersion = "2.1.0-M5"
+  val buildSettings = Defaults.defaultSettings ++ Seq(
+    resolvers += Resolver.sonatypeRepo("snapshots"),
+    resolvers += Resolver.sonatypeRepo("releases"),
+    addCompilerPlugin("org.scalamacros" % "paradise" % paradiseVersion cross CrossVersion.full)
+  )
+}
+
+object MyBuild extends Build {
+  import BuildSettings._
+
+  lazy val root: Project = Project(
+    "root",
+    file("."),
+    settings = buildSettings
+  )
+
+ /* lazy val macros: Project = Project(
+    "macros",
+    file("macros"),
+    settings = buildSettings ++ Seq(
+      libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _),
+      libraryDependencies ++= (
+        if (scalaVersion.value.startsWith("2.10")) List("org.scalamacros" %% "quasiquotes" % paradiseVersion)
+        else Nil
+      )
+    )
+  )
+
+  lazy val core: Project = Project(
+    "core",
+    file("core"),
+    settings = buildSettings
+  ) dependsOn(macros)*/
+}
